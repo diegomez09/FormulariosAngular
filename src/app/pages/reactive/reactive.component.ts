51144d57decee9cf4e712c8c53c9e540d620f477
Diego Gomez <diegofabricioggmail.com>
2020-05-06 16:57:36 -0500

    Formulario #201

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -17,18 +17,40 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid &&
+    this.forma.get('nombre').touched
+  }
+
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid &&
+    this.forma.get('apellido').touched
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo').invalid &&
+    this.forma.get('correo').touched
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
       //Valor por defecto, sincronos,asincronos
-      nombre: ['', ,],
-      apellido: [''],
-      correo: ['',]
+      nombre: ['', [Validators.required,Validators.minLength(5)],],
+      apellido: ['',[Validators.required,Validators.minLength(5)]],
+      //pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+      correo: ['',[Validators.pattern('[a-z0-9._%+-]+@[a-z0-9-]+[.]+[a-z]{2,3}$'),
+                  Validators.required]]
 
     });
   }
 
   guardar(){
     console.log(this.forma);
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control =>{
+        control.markAsTouched();
+      });      
+    }
   }
 
 }
